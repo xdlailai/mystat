f2ce745d6c6df9ff518a7d257c6f01f7d5bd5be1
fly100 <fly100@debian-fly100>
2013-06-30 16:28:17 +0800

    流量统计模块实现

--- mystat.c
+++ mystat.c
@@ -75,7 +75,7 @@ void log_message(char *filename, char *message)
 {
   FILE *logfile;
   logfile=fopen(filename, "a");
-  if(logfile)
+  if(!logfile)
     return;
   fprintf(logfile, "%s\n", message);
   fclose(logfile);
